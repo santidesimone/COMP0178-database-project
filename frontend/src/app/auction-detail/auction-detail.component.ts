451597e2a10d64e6550c5e3ecf70f7210d98edf1
Santiago De Simone <santidesimone@gmail.com>
2024-11-17 19:05:17 +0000

    core functionalitye 4: wip

--- frontend/src/app/auction-detail/auction-detail.component.ts
+++ frontend/src/app/auction-detail/auction-detail.component.ts
@@ -14,10 +14,21 @@ export class AuctionDetailComponent implements OnInit {
   auctionID: string | null = null;
   auction: any;
   data: any;
+  offerAmount: number = 0; // To store the offer amount
 
   constructor(private route: ActivatedRoute) {
 
-
+// Buyers can bid for items 
+    // ==> Just update Bids table with corresponding parameters: BidAmount, BidderID, AuctionID
+// Buyers see the bids other users make as they are received 
+    // ==> Get all bids on Auctions I made a bid for
+            // Shall I communicate through email? I don't think it's "core functionality"
+            // How will the buyer be able to "see the bids other users make as they are received"?
+// The system will manage the auction until the set end time and award the item to the highest bidder.
+    // ==> Get all bids [that loggued user made a bid on] that are due [either for endTime reached, or reservePrice met]
+// The system should confirm to both the winner and seller the outcome of an auction.
+    // ==> Get all bids [that loggued user made a bid on] that are due [either for endTime reached, or reservePrice met]
+            // ==> How each user profile, buyers and seller participating in an auction, will be informed about this outcome?
   }
 
   ngOnInit(): void {
@@ -25,11 +36,42 @@ export class AuctionDetailComponent implements OnInit {
     console.log('Auction ID:', this.auctionID);
     this.data = history.state;
     console.log(this.data)
+    this.offerAmount = this.data.StartingPrice;
+
   }
 
   makeOffer() {
+    console.log('Offer made:', this.offerAmount);
     // Logic for making an offer
     // alert('Offer made!');
+
+    // {
+    //   "BidAmount": this.data.offerAmount,
+    //   // "BidderID": , // either user id (and perform search on backend) or directly send buyer_id if i have it here, on the frontend
+    //   "AuctionID": this.auctionID
+    // }
+    // BidAmount
+    // BidderID
+    // AuctionID
+
+
+    // create logic: if you meet Reserve Price, then you are winner
+
+    //  if winner, update auction ad ended
+
+    //  communicate to both seller and buyer 
+
+    //-- 7. Create the `Bids` Table
+    // CREATE TABLE `Bids` (
+    //     `BidID` INT AUTO_INCREMENT PRIMARY KEY,
+    //     `BidAmount` DECIMAL(10, 2) NOT NULL,
+    //     `BidTime` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    //     `BidderID` INT NOT NULL,
+    //     `AuctionID` INT NOT NULL,
+    //     FOREIGN KEY (`BidderID`) REFERENCES `BuyerDetails`(`BuyerID`),
+    //     FOREIGN KEY (`AuctionID`) REFERENCES `Auctions`(`AuctionID`)
+    // );
+
   }
   
   addToCart() {
